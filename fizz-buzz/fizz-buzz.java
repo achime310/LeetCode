360c33055a15d3682cd99039c6bf19042affe03a
Seo-young Choi <105257804+achime310@users.noreply.github.com>
2022-10-30 10:46:33 +0900

    Time: 4 ms, Memory: 48.8 MB - LeetHub

---
+++ fizz-buzz/fizz-buzz.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<String>();
+        
+        for(int i=1; i<n+1; i++){
+            if(i%3==0){
+                if(i%5==0){
+                    list.add("FizzBuzz");
+                }else{
+                    list.add("Fizz");
+                }
+            }else if(i%5==0){
+                list.add("Buzz");
+            }else{
+                list.add(i+"");
+            }
+        }
+        return list;
+    }
+}
